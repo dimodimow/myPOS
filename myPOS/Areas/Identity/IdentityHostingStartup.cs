fe6b367fec28a17e2dbf8db36875f97d4ff09487
Dimarata <ivanov_dimo@abv.bg>
2020-06-21 17:44:01 +0300

    Scaffold Identity

---
+++ myPOS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using myPOS.Data.Context;
+using myPOS.Entities;
+
+[assembly: HostingStartup(typeof(myPOS.Web.Areas.Identity.IdentityHostingStartup))]
+namespace myPOS.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
